2dac0c9f6afeba8a6995a582030eb53b9a31d30b
Derell <derelljenkins@gmail.com>
2021-03-06 17:12:10 -0600

    New sql query file

---
+++ sql-challenge-queries.sql
@@ -0,0 +1,36 @@
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+Select *
+From employee 
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+Select *
+From employee 
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+Select *
+From employee 
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+Select *
+From employee 
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+Select *
+From employee 
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+Select *
+From employee 
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+Select *
+From employee 
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+Select *
+From employee 
+
+
+
+
