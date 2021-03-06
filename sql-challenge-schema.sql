ad0cb9dbd54c987f1442ea907231564fd8629a80
Derell <derelljenkins@gmail.com>
2021-03-06 17:09:50 -0600

    Updated name and removed queries

---
+++ sql-challenge-schema.sql
@@ -0,0 +1,57 @@
+-- Delete any listed table
+DROP TABLE departments;
+DROP TABLE dept_emp;
+DROP TABLE employee;
+DROP TABLE titles;
+DROP TABLE salaries;
+DROP TABLE dept_manager;
+
+
+
+
+-- Create a departments table
+CREATE TABLE departments (
+    dept_no VARCHAR(10) NOT NULL PRIMARY KEY,
+	dept_name varchar(255) NOT NULL
+);
+
+-- Create a dept_emp table
+CREATE TABLE dept_emp (
+	emp_no int NOT NULL,
+    dept_no VARCHAR(10)  NOT NULL,
+	PRIMARY KEY (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+CREATE TABLE customer_phone (
+    id SERIAL,
+    phone VARCHAR(30) NOT NULL,
+    customer_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+
+-- Create a employee table
+CREATE TABLE employee (
+	emp_no int NOT NULL PRIMARY KEY,
+ 	emp_title_id VARCHAR(30) NOT NULL,
+	birth_date Date NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date Date NOT Null	
+);
+
+-- Create a titles table
+CREATE TABLE titles (
+    title_id VARCHAR(10)  NOT NULL,
+    title varchar(255) NOT NULL
+);
+
+-- Create a salaries table
+CREATE TABLE salaries (
+    emp_no VARCHAR(10)  NOT NULL,
+    salary varchar(255) NOT NULL
+);
