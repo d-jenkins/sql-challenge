f5708a7012a20b2db15f8cdc6a5e4877b77c5607
Derell <derelljenkins@gmail.com>
2021-03-06 19:07:52 -0600

    Updated Manager query

--- sql-challenge-queries.sql
+++ sql-challenge-queries.sql
@@ -6,20 +6,35 @@ Select e.emp_no,
 	e.sex,
 	s.salary
 From employee as e
-INNER JOIN salaries as s ON
-e.emp_no = s.emp_no
+INNER JOIN salaries as s ON e.emp_no = s.emp_no
 
 
 --List first name, last name, and hire date for employees who were hired in 1986.
-Select e.first_name, e.last_name, e.hire_date
+Select e.first_name, 
+	e.last_name, 
+	e.hire_date
 From employee as e
 Where hire_date  >= '1986-01-01 00:00:00' 
 AND  hire_date <  '1987-01-01 00:00:00'
 
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-Select *
-From employee 
+Select d.dept_no, 
+	d.dept_name,
+	de.emp_no,
+	e.last_name,
+	e.first_name
+From departments as d 
+INNER JOIN dept_emp as de ON d.dept_no = de.dept_no
+INNER JOIN employee as e ON de.emp_no = e.emp_no
+where de.emp_no IN
+	( 
+		Select emp_no
+		FROM dept_manager
+
+	);
+
+
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
 Select *
